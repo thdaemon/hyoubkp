21811a77d9a3fb9bb42577cd193406f1a5f9a655
Eric Tian <thxdaemon@gmail.com>
2024-10-04 00:37:01 +0800

    hyoubkp_mobile_ios: add 'cargo:rerun-if-changed' instructions

--- crates/hyoubkp_mobile_ios/build.rs
+++ crates/hyoubkp_mobile_ios/build.rs
@@ -7,6 +7,7 @@ fn main() {
         Err(_) => String::from("unknown"),
     };
     println!("cargo:rustc-env=HM_GIT_HASH={}", git_hash);
+    println!("cargo:rerun-if-changed=../../.git/HEAD");
 
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
@@ -23,5 +24,7 @@ fn main() {
         println!("cargo:rustc-link-lib=framework=Foundation");
         println!("cargo:rustc-link-lib=framework=CoreGraphics");
         println!("cargo:rustc-link-lib=framework=UIKit");
+
+        println!("cargo:rerun-if-changed=./libui");
     }
 }
